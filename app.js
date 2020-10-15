fa3f74829df61578bbec43557169cdf31cd08b15
SlaIN666 <72605552+SlaIN666@users.noreply.github.com>
2020-10-15 12:23:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ app.use(express.json({limit: '1mb'}))
 
 app.post('/send', (req, res) => {
   let data = req.body.data
-  let sum = []
-
-  data.split('').map(el => {
-      el = el.toLowerCase()
-      sum[el] = !sum[el] ? 1 : sum[el]+1
-  })
-
-  let str =[]
-  for (let [key, value] of Object.entries(sum)) {
-      str += [value, key].join('')      
+  let sum = data.split('')
+  let c = 1
+  
+  for (let i = 0; i < sum.length; i++) {
+      if(sum[i+1] == sum[i]) {
+          c++
+      } else {
+          sum[i] = c+sum[i]
+          c = 1
+      }
   }
   
-  res.json(str)
-
+  sum = sum.join('').match(/[1-9][a-z]/gi).join('')
+  
+  res.json(sum)
 })
 
 app.get('*', (req, res) => {
